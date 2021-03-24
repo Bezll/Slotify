ee7a9b84f7f1a3be3dd6de99e140ef41532d85d2
Bezll <60795516+Bezll@users.noreply.github.com>
2021-03-24 16:25:05 +0000

    Refactoring / creating main component

--- index.php
+++ index.php
@@ -1,169 +1,34 @@
 <!DOCTYPE html>
 
-<?php
-include("includes/config.php");
+<?php include("includes/header.php"); ?>
 
-//session_destroy();
+<h1 class="pageHeadingBig"> You Might Also Like</h1>
 
-if(isset($_SESSION['userLoggedIn'])) {
-	$userLoggedIn = $_SESSION['userLoggedIn'];
-} else {
-	header("Location: register.php");
-}
-
-?>
-
-<head>
-
-	<title>Welcome to Slotify!</title>
-
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-
-</head>
-
-<body>
-
-	<div id="mainContainer">
-
-		<div id="topContainer">
-			
-			<div id="navBarContainer">
-				<nav class="navBar">
-					<a href="index.php" class="logo">
-						<img src="assets/images/icons/logo.png">
-					</a>
-
-					<div class="group">
-						
-						<div class="navItem">
-							<a href="search.php" class="navItemLink">Search
-								<img src="assets/images/icons/search.png" class="icon" alt="Search">
-							</a>
-						</div>
-
-					</div>
-
-					<div class="group">
-
-						<div class="navItem">
-							<a href="browse.php" class="navItemLink">Browse</a>
-						</div>
-
-						<div class="navItem">
-							<a href="yourMusic.php" class="navItemLink">Your Music</a>
-						</div>
-
-						<div class="navItem">
-							<a href="yourProfile.php" class="navItemLink">Your Profile</a>
-						</div>
-						
-					</div>
-
-				</nav>
-			</div>
-
-		</div>
+<div class="gridViewContainer">
 	
-		<div id="nowPlayingBarContainer">
-			
-			<div id="nowPlayingBar">
-				
-				<div id="nowPlayingLeft">
+	<?php
+		$albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+
+		while($row = mysqli_fetch_array($albumQuery)) {
+
+			echo "<div class='gridViewItem'>
+				<a href='album.php?id=" . $row['id'] . "'>
+					<img src='" . $row['artworkPath'] . "'>
 					
-					<div class="content">
-						
-						<span class="albumLink">
-							<img src="https://jooinn.com/images/square-4.jpg" class="albumArtwork">
-						</span>
+					<div class='gridViewInfo'>"
+						. $row['title'] .
+					"</div>
+				</a>
 
-						<div class="trackInfo">
-							
-							<span class="trackName">
-								<span>Happy Birthday</span>
-							</span>
+			</div>";
+		}
+	?>
 
-							<span class="artistName">
-								<span>Reece Kenny</span>
-							</span>
+</div>
 
-						</div>
 
-					</div>
+<?php include("includes/footer.php"); ?>
 
-				</div>
 
-				<div id="nowPlayingCenter">
-					
-					<div class="content playerControls"></div>
 
-						<div class="buttons">
-							
-							<button class="controlButton shuffle" title="Shuffle Button">
-								<img src="assets/images/icons/shuffle.png" alt="Shuffle">
-							</button>
 
-							<button class="controlButton previous" title="Previous Button">
-								<img src="assets/images/icons/previous.png" alt="Previous">
-							</button>
-
-							<button class="controlButton play" title="Play Button">
-								<img src="assets/images/icons/play.png" alt="Play">
-							</button>
-
-							<button class="controlButton pause" title="Pause Button" style="display:none;">
-								<img src="assets/images/icons/pause.png" alt="Pause">
-							</button>
-
-							<button class="controlButton next" title="Next Button">
-								<img src="assets/images/icons/next.png" alt="Next">
-							</button>
-
-							<button class="controlButton repeat" title="Repeat Button">
-								<img src="assets/images/icons/repeat.png" alt="Repeat">
-							</button>
-
-						</div>
-
-						<div class="playbackBar">
-							
-							<span class="progressTime current">0.00</span>
-
-							<div class="progressBar">
-								<div class="progressBarBg">
-									<div class="progress"></div>
-								</div>
-							</div>
-							
-							<span class="progressTime remaining">0.00</span>
-
-						</div>
-
-				</div>
-
-				<div id="nowPlayingRight">
-					<div class="volumeBar">
-						
-						<button class="controlButton volume" title="Volume Button">
-							<img src="assets/images/icons/volume.png" alt="Volume">
-						</button>
-
-						<div class="progressBar">
-							<div class="progressBarBg">
-								<div class="progress"></div>
-							</div>
-
-						</div>
-
-					</div>
-
-				</div>
-
-			</div>
-
-		</div>
-
-	</div>
-
-</body>
-
-</html>
